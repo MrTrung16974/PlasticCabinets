15827dc345154b0cbdaf8b3f7452b27e8d445f3a
Nguyễn Tiến Trung <mr.trung0104@gmail.com>
2020-03-16 16:48:23 +0700

    Update

---
+++ src/main/java/com/example/plasticcabinets/PlasticcabinetsApplication.java
@@ -0,0 +1,13 @@
+package com.example.plasticcabinets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlasticcabinetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlasticcabinetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/plasticcabinets/PlasticcabinetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plasticcabinets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlasticcabinetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
